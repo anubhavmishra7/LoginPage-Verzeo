6608212007eb5c7e1fc8df897a2fbda67485008d
anubhavmishra7 <anubhavcv77@gmail..com>
2021-07-23 12:38:39 +0530

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,210 @@
+//import 'dart:html';
+
+import 'package:demoapp/pages/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class LoginPage extends StatefulWidget {
+  // const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  void validate() {
+    if (formkey.currentState!.validate()) {
+      print('Great');
+    } else {
+      print('Error');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.greenAccent,
+      child: Column(children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 100.0),
+          child: Container(
+            alignment: Alignment.topCenter,
+            child: Expanded(
+                child: Column(
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, Myroutes.homeroute);
+                  },
+                  child: Container(
+                    // alignment: Alignment(5, 0),
+
+                    child: Center(
+                      child: Text(
+                        'X',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+
+                    margin: EdgeInsets.only(right: 325, top: 10),
+                    height: 25,
+                    width: 25,
+                    decoration: BoxDecoration(
+                        color: Colors.greenAccent, shape: BoxShape.circle),
+
+                    //child: Icon(Icons.close),
+                  ),
+                ),
+                Container(
+                  child: Center(
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 52,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  margin: EdgeInsets.only(
+                      top: 70, bottom: 100, left: 100, right: 100),
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                      color: Colors.greenAccent, shape: BoxShape.circle),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 10, left: 8, right: 8),
+                  child: Form(
+                    child: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: (InputDecoration(
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.all(0),
+                            child: Icon(Icons.mail),
+                          ),
+                          hintText: 'Email',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(40)))),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Required";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                Form(
+                  child: Container(
+                    padding: EdgeInsets.only(top: 10, left: 8, right: 8),
+                    child: TextFormField(
+                      obscureText: true,
+                      decoration: (InputDecoration(
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.all(0),
+                            child: Icon(Icons.lock),
+                          ),
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(40)))),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Required";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 70, left: 15, right: 15),
+                  child: TextButton(
+                      child: Text('LOGIN', style: TextStyle(fontSize: 20)),
+                      style: TextButton.styleFrom(
+                          primary: Colors.white,
+                          //onSurface: Colors.white,
+                          //shadowColor: Colors.black,
+                          backgroundColor: Colors.greenAccent,
+                          elevation: 5,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          minimumSize: Size(double.infinity, 30),
+                          //minimumSize: Size(400, 100),
+
+                          padding: EdgeInsets.only(
+                              left: 0, right: 0, top: 15, bottom: 15)),
+                      onPressed: validate
+                      //Navigator.pushNamed(context, Myroutes.loginroute);
+
+                      ),
+                ),
+              ],
+            )),
+
+            width: double.infinity,
+            height: 680,
+            //constraints: BoxConstraints.expand(),
+
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+
+              //color: Colors.greenAccent,
+            ),
+          ),
+        ),
+
+        // Container(
+        //   decoration: BoxDecoration(),
+
+        // )
+
+        //Container 1
+
+        // Image.asset("assets/images/login.png"),
+        // Text("This right here is the Second Screen"),
+        // SizedBox(
+        //   height: 40,
+        // ),
+        //Padding(padding:EdgeInsetsGeometry ),
+        // Text(
+        //   "THANK YOU FOR SUBMITTING !!",
+        //   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        // ),
+        //SizedBox(
+        // height: 40,
+        // ),
+
+        //Container 2
+        // Center(
+        //   child: Text(
+        //     "Login Page",
+        //     style: TextStyle(
+        //         fontSize: 40,
+        //         fontStyle: FontStyle.italic,
+        //         fontWeight: FontWeight.bold,
+        //         color: Colors.blue),
+        //     textScaleFactor: 1.0,
+        //   ),
+      ]),
+    );
+  }
+}
+
+// Inserting Text Widget
+//    child: Center(
+//         child: Text(
+//           "Login Page",
+//           style: TextStyle(
+//               fontSize: 40,
+//               fontStyle: FontStyle.italic,
+//               fontWeight: FontWeight.bold,
+//               color: Colors.blue),
+//           textScaleFactor: 1.0,
+//         ),
+//       ),
---
+++ lib/pages/routes.dart
@@ -0,0 +1,5 @@
+class Myroutes {
+  static String loginroute = "/";
+  static String homeroute = "/home";
+  static String registerroute = "/register";
+}
